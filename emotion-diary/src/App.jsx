52e8f36155f61741a2c4561c942369f5fb10ab2b
tjdux <tjduxxx@gmail.com>
2025-07-02 22:46:50 +0900

    폰트, 이미지, 레이아웃 설정

--- emotion-diary/src/App.jsx
+++ emotion-diary/src/App.jsx
@@ -5,6 +5,8 @@ import Diary from "./pages/Diary";
 import New from "./pages/New";
 import NotFound from "./pages/Notfound";
 
+import { getEmotionImage } from "./util/get-emtion-image";
+
 // 1. "/": 모든 일기 조회하는 Home 페이지
 // 2. "/new": 새로운 일기 작성하는 New 페이지
 // 3. "/diary": 일기를 상세히 조회하는 Diary 페이지
@@ -16,6 +18,13 @@ function App() {
   };
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
